d11e0ceb98db05df6d136cc4f14d4bfe736b25e0
KubaG1974 <kuba.gorzynski@gmail.com>
2022-11-06 10:47:34 +0100

    Poprawki kodu

--- guessing_game.py
+++ guessing_game.py
@@ -1,5 +1,6 @@
 import math
 import random
+TIME_TO_PLAY = 10
 a = [] #tworzymy pustą listę i przypisujemy ją do zmiennej a
 a.append(random.randint(1, 99)) #dodawanie na koniec listy 'a' losowo utworzonej liczby o wartości 1-99, wykonanej 10 razy
 a.append(random.randint(1, 99))
@@ -11,7 +12,7 @@ a.append(random.randint(1, 99))
 a.append(random.randint(1, 99))
 a.append(random.randint(1, 99))
 a.append(random.randint(1, 99))
-for i in range(10): #tworzymy pętle która będzie powtarzana w przedziale od 0 do 9
+for i in range(TIME_TO_PLAY): #tworzymy pętle która będzie powtarzana w przedziale od 0 do 9
     g = int(input("Enter an integer from 1 to 99: ")) #pobieramy wpisywaną wartość od użytkownika, rzutujemy wartość na typ integer i przypisujemy zmienną
     while a[i] != g: #rozpoczynamy pętle która trwa dopóki element i z listy a jest różny od wartości pobranej od użytkownika
         if g < a[i]: #jeżeli wartość pobrana od użytkownika jest mniejsza od kolejnego elementu z listy a
